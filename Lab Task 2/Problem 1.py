eb4af0de2c7b7c955148160bd29b6a834ac5f103
mashrurahmed01 <23-50500-1@student.aiub.edu>
2026-06-14 12:21:26 +0600

    Lab Task 2

---
+++ Lab Task 2/Problem 1.py
@@ -0,0 +1,6 @@
+num1 = 85
+num2 = 75
+swap = num2
+num2 = num1
+num1 = swap
+print(num1, num2)
---
+++ Lab Task 2/Problem 2.py
@@ -0,0 +1,11 @@
+print("Enter point 1 X value: ")
+x1 = int(input ())
+print("Enter point 1 Y value: ")
+y1 = int(input ())
+print("Enter point 2 X value: ")
+x2 = int(input ())
+print("Enter point 2 Y value: ")
+y2 = int(input ())
+
+distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(distance)
